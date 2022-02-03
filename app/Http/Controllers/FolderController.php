c6f3a227f5b6f33afa765fdfc5c200be448f332e
ShotaKinekawa <mmmicap.kinetch@gmail.com>
2022-02-03 11:04:54 +0900

    第5章終了

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateFolder;
+use App\Models\Folder;
+use Illuminate\Http\Request;
+
+class FolderController extends Controller
+{
+    //
+    public function showCreateForm()
+    {
+        # code...
+        return view('folders/create');
+    }
+
+    public function create(CreateFolder $request)
+    {
+        # code...
+
+        // フォルダモデルのインスタンスを作成する。
+        $folder = new Folder();
+
+        // タイトルに入力値を代入する。
+        $folder->title = $request->title;
+
+        // インスタンスの状態をデータベースに書き込む
+        $folder->save();
+
+        return redirect()->route('tasks.index', ['id' => $folder->id,]);
+    }
+}
