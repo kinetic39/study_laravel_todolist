bb4e35cafe11c2edb5a3a49829bf889afb366b0f
ShotaKinekawa <mmmicap.kinetch@gmail.com>
2022-02-07 10:16:39 +0900

    認証周りのインストール完了

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::post('folders/create', [FolderController::class, 'create']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
